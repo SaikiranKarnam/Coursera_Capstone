8b0b7034e9ae557820a263a5157b84cc34078b0f
SaikiranKarnam <52953704+SaikiranKarnam@users.noreply.github.com>
2019-07-16 14:22:24 +0530

    Link
    https://eu-gb.dataplatform.cloud.ibm.com/analytics/notebooks/v2/21a825bc-d3ed-4473-81e4-a60c7e8fd5cd/view?access_token=f9af0048ff6b44e0056356e5514a04426767cfea083fda78004a8fccdf538259

---
+++ Capstone Project.py
@@ -0,0 +1,12 @@
+
+# coding: utf-8
+
+# <h1>This Notebook is exclusively built for Capstone Project</h1>
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+print("Hello Capstone Project Course!")
+
